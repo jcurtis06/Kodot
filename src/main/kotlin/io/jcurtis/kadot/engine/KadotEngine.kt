81d14f355fb09e38a3ad0e0b505a84ea13795ef9
ElijahBradshaw <eli.b@realassist.ai>
2023-04-27 10:51:57 -0400

    new branch

--- src/main/kotlin/io/jcurtis/kadot/engine/KadotEngine.kt
+++ src/main/kotlin/io/jcurtis/kadot/engine/KadotEngine.kt
@@ -1,9 +1,9 @@
 package io.jcurtis.kadot.engine
 
 import io.jcurtis.kadot.engine.nodes.Node
+import io.jcurtis.kadot.engine.nodes.Root
 import io.jcurtis.kadot.engine.nodes.graphical.GraphicalNode
 import io.jcurtis.kadot.engine.nodes.physics.CollisionBody
-import io.jcurtis.kadot.engine.nodes.physics.StaticBody
 import java.awt.Graphics
 import java.awt.Graphics2D
 import javax.swing.JFrame
@@ -11,23 +11,22 @@ import javax.swing.JPanel
 import javax.swing.SwingUtilities
 
 @Suppress("MemberVisibilityCanBePrivate")
-class KadotEngine(
-    private var title: String = "Kadot Engine",
-    private var width: Int = 800,
-    private var height: Int = 600,
-    maxFrameRate: Int = 240
-) : JPanel(), Runnable {
-    private val thread: Thread = Thread(this)
-
-    private var targetTime = 1000 / maxFrameRate
-    private var lastTime: Long = System.currentTimeMillis()
-
-
-    companion object {
+class KadotEngine {
+    companion object : JPanel(), Runnable {
+        // You can add getters/setters if you want, but I feel like making this static is better.
+        var title: String = "Kadot Engine"
+        var screenWidth: Int = 800
+        var screenHeight: Int = 600
+        var maxFrameRate: Int = 240
+        // Why not make root static? Because maybe you want to have multiple game scenes active at once.
+        var root: Root = Root()
+        var targetTime = 1000 / maxFrameRate
+        var lastTime: Long = System.currentTimeMillis()
         var nodes = mutableListOf<Node>()
         var colliderNodes = mutableListOf<CollisionBody>()
         var delta: Double = 0.0
 
+        val thread: Thread = Thread(this)
         fun registerNode(node: Node) {
             nodes.add(node)
 
@@ -35,68 +34,68 @@ class KadotEngine(
                 colliderNodes.add(node)
             }
         }
-    }
 
-    fun init() {
-        for (node in nodes) {
-            node.ready()
-        }
-
-        SwingUtilities.invokeLater {
-            val frame = JFrame(title)
-            frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-            frame.setSize(width, height)
-            frame.isResizable = false
-            frame.setLocationRelativeTo(null)
-            frame.isVisible = true
-
-            frame.add(this)
-            frame.addKeyListener(Input())
-        }
-
-        thread.start()
-    }
-
-    private fun update() {
-        for (node in nodes) {
-            node.update((delta))
-        }
-        repaint()
-    }
-
-    override fun paintComponent(g: Graphics?) {
-        super.paintComponent(g)
-
-        val g2d = g as Graphics2D
-        for (n in nodes) {
-            if (n is GraphicalNode) {
-                n.draw(g2d, this)
+        fun init() {
+            for (node in nodes) {
+                node.ready()
             }
+
+            SwingUtilities.invokeLater {
+                val frame = JFrame(title)
+                frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+                frame.setSize(screenWidth, screenHeight)
+                frame.isResizable = false
+                frame.setLocationRelativeTo(null)
+                frame.isVisible = true
+
+                frame.add(this)
+                frame.addKeyListener(Input())
+            }
+
+            thread.start()
         }
-    }
 
-    override fun run() {
-        var currentTime: Long
-        var elapsedTime: Long
-        var sleepTime: Long
+        private fun update() {
+            for (node in nodes) {
+                node.update((delta))
+            }
+            repaint()
+        }
 
-        while (true) {
-            currentTime = System.currentTimeMillis()
-            elapsedTime = currentTime - lastTime
-            delta = elapsedTime / 1000.0
+        override fun paintComponent(g: Graphics?) {
+            super.paintComponent(g)
 
-            if (elapsedTime < targetTime) {
-                sleepTime = targetTime - elapsedTime
-
-                try {
-                    Thread.sleep(sleepTime)
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
+            val g2d = g as Graphics2D
+            for (n in nodes) {
+                if (n is GraphicalNode) {
+                    n.draw(g2d, this)
                 }
             }
+        }
 
-            update()
-            lastTime = currentTime
+        override fun run() {
+            var currentTime: Long
+            var elapsedTime: Long
+            var sleepTime: Long
+
+            while (true) {
+                currentTime = System.currentTimeMillis()
+                elapsedTime = currentTime - lastTime
+                delta = elapsedTime / 1000.0
+
+                if (elapsedTime < targetTime) {
+                    sleepTime = targetTime - elapsedTime
+
+                    try {
+                        Thread.sleep(sleepTime)
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+                }
+
+                update()
+                lastTime = currentTime
+            }
         }
     }
 }
---
+++ src/main/kotlin/io/jcurtis/kadot/engine/example/Yes.kt
@@ -0,0 +1,9 @@
+import io.jcurtis.kadot.engine.KadotEngine
+import io.jcurtis.kadot.engine.example.Mario
+
+fun main() {
+    KadotEngine.root.addChild(Mario())
+
+    KadotEngine.init()
+}
+
