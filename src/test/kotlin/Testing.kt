165854c0d3aace5348962456c738b1ebf1d6ada4
jcurtis06 <06jcurtis@gmail.com>
2023-04-28 22:16:20 -0400

    added improved starter code

--- src/test/kotlin/Testing.kt
+++ src/test/kotlin/Testing.kt
@@ -1,12 +1,19 @@
 import io.jcurtis.kadot.engine.Kodot
 import io.jcurtis.kadot.engine.nodes.Sprite
+import io.jcurtis.kadot.engine.nodes.graphical.ui.Text
 
 fun main() {
     val icon = Sprite("images/icon.png")
+    val text = Text("Hello Kodot!")
 
-    icon.position.x = 100.0
+    icon.position.x = 141.0
+    icon.position.y = 212.0
+
+    text.position.x = 350.0
+    text.position.y = 50.0
 
     Kodot.root.addChild(icon)
+    Kodot.root.addChild(text)
 
     Kodot.init()
 }
